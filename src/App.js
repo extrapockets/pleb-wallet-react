0fe94b461460fa0e068d024dc7fa649f8ba83035
extrapockets <extrapockets@proton.me>
2023-04-17 21:28:49 -0500

    Example rebuilding deployment

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <h1>Hello world!</h1>
       <h2>This is my first React project.</h2>
       <h3>I am learning every day.</h3>
+      <h4>lorem ipsum</h4>
     </div>
   );
 }
