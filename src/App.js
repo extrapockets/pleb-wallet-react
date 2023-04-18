fe325a90b411cb825f281b49ac4c1d446cfb8798
extrapockets <extrapockets@proton.me>
2023-04-17 21:35:47 -0500

    Added a goofy message

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h2>This is my first React project.</h2>
       <h3>I am learning every day.</h3>
       <h4>lorem ipsum</h4>
+      <h5>Hello my friend, would you like a slice of fresh orange from one of my many pockets?</h5>
     </div>
   );
 }
